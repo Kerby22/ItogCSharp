a2ce2e0a273e32de3bf1d79370eed36a94e2bd4a
Kerby22 <test20072009@yandex.ru>
2023-01-13 01:58:14 +0700

    code for zadacha 64

--- zadacha64/Program.cs
+++ zadacha64/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = 5;//определяем первоначальное значение
+void NumCnt (int num)
+{Console.Write(num);
+NumCnt (num - 1);
+}
+NumCnt(num);
