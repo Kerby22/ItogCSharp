c494de5b78bcb0d552dc7151e2b02c616b3585bf
Kerby22 <test20072009@yandex.ru>
2023-01-13 07:55:12 +0700

    remake blok-shema i telo progr

--- zadacha68/Program.cs
+++ zadacha68/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.Clear();
-int ChM = 2;
-int ChN = 3;
+int ChM = 3;
+int ChN = 6;
+int AKK (int ChM, int ChN)
+{
+    if (ChM == 0) return ChN + 1;
+if (ChM != 0 && ChN == 0) return AKK(ChM - 1, 1);
+if (ChM > 0 && ChN > 0) return AKK(ChM - 1, AKK(ChM, ChN - 1));
+return AKK (ChM, ChN);
+}
+Console.WriteLine($"функция Аккермана для чисел {ChM} и {ChN} = {AKK(ChM, ChN)}");
